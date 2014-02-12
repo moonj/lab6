34e52fedea9335c214571c43cd663a99eac5da1f
Jay Moon <jaymoon@stanford.edu>
2014-02-12 15:18:49 -0800

    finished the lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -8,16 +8,15 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
-	$('.project a').click(addProjectDetails);
-
-	$('#colorBtn').click(randomizeColors);
-}
+ function initializePage() {
+ 	$('.project a').click(addProjectDetails);
+ 	$('#colorBtn').click(randomizeColors);
+ }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
-function addProjectDetails(e) {
+ function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
 
@@ -25,6 +24,7 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	$.get('/project/' + idNumber, getProject);
 
 	console.log("User clicked on project " + idNumber);
 }
@@ -33,6 +33,25 @@ function addProjectDetails(e) {
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
-function randomizeColors(e) {
-	console.log("User clicked on color button");
-}
+ function randomizeColors(e) {
+ 	console.log("User clicked on color button");
+
+ 	$.get('/palette', getPalette);
+ }
+
+ function getProject(result) {
+ 	$('#project' + result['id'] + ' .details').html(
+ 		'<img src="' + result['image'] + '" class = "detailsImage">' +
+ 		'<h2>' + result['date'] + '</h2>' +
+ 		result['summary']
+ 		);
+ }
+
+ function getPalette(result) {
+ 	var colors = result['colors']['hex'];
+ 	$('body').css('background-color', colors[0]);
+ 	$('.thumbnail').css('background-color', colors[1]);
+ 	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+ 	$('p').css('color', colors[3]);
+ 	$('.project img').css('opacity', .75);
+ }
